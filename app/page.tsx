89cdfaff4749740aa521d8943dd6ee230f3282e8
UzuLaura <laura.uzusiene@gmail.com>
2025-11-09 13:55:09 +0200

    Add mobile only support message and logic, base container styles.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,33 @@
-import Image from "next/image";
+"use client";
+import {useState, useEffect, JSX} from "react";
 
-export default function Home() {
-  return (
-    <div>
-      Initial Page
-    </div>
-  );
+export default function Home(): JSX.Element | null {
+    const [isMobile, setIsMobile] = useState<boolean | null>(null);
+    const maxAllowedScreenWidth = 768;
+
+    useEffect((): (() => void) => {
+        function handleResize(): void {
+            setIsMobile(window.innerWidth <= maxAllowedScreenWidth);
+        }
+
+        handleResize();
+        window.addEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
+    if (isMobile === null) return null;
+
+    if (!isMobile) {
+        return (
+            <div className="flex items-center justify-center min-h-screen text-center p-6 text-lg font-medium">
+                This page supports mobile devices only.
+            </div>
+        );
+    }
+
+    return (
+        <div className="container">
+            Initial Page
+        </div>
+    );
 }
